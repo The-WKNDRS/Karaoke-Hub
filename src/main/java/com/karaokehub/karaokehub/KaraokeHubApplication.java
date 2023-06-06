6e209085b0734aa9b649ae35de53a3a264aec186
Joe Beltran <jab102191@gmail.com>
2023-06-06 10:34:06 -0500

    init repo

---
+++ src/main/java/com/karaokehub/karaokehub/KaraokeHubApplication.java
@@ -0,0 +1,13 @@
+package com.karaokehub.karaokehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaraokeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaraokeHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karaokehub/karaokehub/KaraokeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karaokehub.karaokehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaraokeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
