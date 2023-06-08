303c92c5d82acd34f0cb7e9e2be420d20174f5ec
isaacachenbach <isaac.achenbach1@gmail.com>
2023-06-08 16:33:16 -0500

    made sec config

---
+++ src/main/java/com/karaokehub/karaokehub/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.karaokehub.karaokehub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/search", "/create-venue", "/user-profile", "/venue-profile").authenticated()
+                .requestMatchers("/register", "/login", "/index").permitAll()
+                .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+        );
+
+        http.formLogin((form) -> form.loginPage("/login").defaultSuccessUrl("/index"));
+        http.logout((form) -> form.logoutSuccessUrl("/login"));
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
