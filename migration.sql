3b5780ce6513503d127bb33dd1bc70ccb90c82dc
Joe Beltran <jab102191@gmail.com>
2023-06-08 15:13:42 -0500

    update migration again with JBDC

--- migration.sql
+++ migration.sql
@@ -2,44 +2,28 @@ DROP DATABASE IF EXISTS karaoke_hub;
 CREATE DATABASE IF NOT EXISTS karaoke_hub;
 USE karaoke_hub;
 
-create table venue (
-                       id  int unsigned auto_increment primary key,
-                       name     varchar(80)  null,
-                       location varchar(255) null,
-                       yelp_id  varchar(80)  unique
-);
-create table users (
-                       id       int unsigned auto_increment primary key,
-                       username varchar(60)  not null,
-                       email    varchar(255) not null,
-                       password varchar(255) not null,
-                       venue_id int unsigned null,
-                       constraint users_venue_id_fk foreign key (venue_id) references venue (id)
+create table venue
+(
+    id       bigint auto_increment
+        primary key,
+    location varchar(100) not null,
+    name     varchar(100) not null,
+    yelp_id  bigint       null
 );
 
-create table comments
+create table users
 (
-    id       int unsigned auto_increment
+    id       bigint auto_increment
         primary key,
-    content  text         not null,
-    user_id  int unsigned not null,
-    venue_id int unsigned not null,
-    constraint comments_users_id_fk
-        foreign key (user_id) references users (id),
-    constraint comments_venue_yelp_id_fk
-        foreign key (venue_id) references venue (id)
-);
-
-create table events
-(
-    id            int unsigned auto_increment
-        primary key,
-    day_of_week   varchar(20)  null,
-    name          varchar(80)  not null,
-    description   text         null,
-    dj            varchar(60)  null,
-    venue_id int unsigned null,
-    constraint events_venue_yelp_id_fk
+    email    varchar(255) not null,
+    password varchar(255) not null,
+    username varchar(60)  not null,
+    venue_id bigint       null,
+    constraint UK_i213kuuy951gve82e7xaadj6s
+        unique (venue_id),
+    constraint UK_r43af9ap4edm43mmtq01oddj6
+        unique (username),
+    constraint FKmhhw40hqa0jgeirc26fsnqv80
         foreign key (venue_id) references venue (id)
 );
 
