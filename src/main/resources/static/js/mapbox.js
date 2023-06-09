06530ae94d4df26ef902320e70cab49ae55f3a4a
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-06-09 13:50:35 -0500

    map center SA

--- src/main/resources/static/js/mapbox.js
+++ src/main/resources/static/js/mapbox.js
@@ -2,6 +2,6 @@ mapboxgl.accessToken = mapKey;
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
-    center: [4.895168, 52.370216],
+    center: [-98.495141, 29.4246],
     zoom: 10
 });
