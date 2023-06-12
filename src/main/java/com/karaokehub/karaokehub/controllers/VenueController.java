17538f79d528a0297fdf937f00b8cca890ba5e54
Joe Beltran <jab102191@gmail.com>
2023-06-12 14:42:45 -0500

    backend for venues

--- src/main/java/com/karaokehub/karaokehub/controllers/VenueController.java
+++ src/main/java/com/karaokehub/karaokehub/controllers/VenueController.java
@@ -1,5 +1,7 @@
 package com.karaokehub.karaokehub.controllers;
+import com.karaokehub.karaokehub.models.Event;
 import com.karaokehub.karaokehub.models.Venue;
+import com.karaokehub.karaokehub.repository.EventRepository;
 import com.karaokehub.karaokehub.repository.VenueRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,25 +13,34 @@ import org.springframework.web.bind.annotation.PostMapping;
     public class VenueController {
 
         private final VenueRepository venueDao;
-        public VenueController(VenueRepository venueDao) {
+        private final EventRepository eventDao;
+
+        public VenueController(VenueRepository venueDao, EventRepository eventDao) {
             this.venueDao = venueDao;
+            this.eventDao = eventDao;
         }
 
         @GetMapping("/create-venue")
         public String createVenue(Model model) {
             model.addAttribute("venue", new Venue());
+            model.addAttribute("event", new Event());
             return "/create-venue";
         }
+
         @PostMapping("/create-venue")
-        public String createVenue(@ModelAttribute Venue venue) {
+        public String createVenue(@ModelAttribute Venue venue, @ModelAttribute Event event) {
             venueDao.save(venue);
+            eventDao.save(event);
             return "redirect:/venue-profile";
         }
-    @GetMapping("/venue-profile")
-    public String venueProfile() {
-        return "/venue-profile";
-    }
 
-    @GetMapping("/search-venue")
-    public String searchVenue() { return "/search"; }
+        @GetMapping("/venue-profile")
+        public String venueProfile() {
+            return "/venue-profile";
+        }
+
+        @GetMapping("/search-venue")
+        public String searchVenue() {
+            return "/search";
+        }
 }
--- src/main/java/com/karaokehub/karaokehub/models/Venue.java
+++ src/main/java/com/karaokehub/karaokehub/models/Venue.java
@@ -20,8 +20,40 @@ public class Venue {
     @Column
     private long yelp_id;
 
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "venue")
+    private List<Event> events;
+
     public Venue() {
     }
+
+    public Venue(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public Venue(String name, String location, List<Event> events) {
+        this.name = name;
+        this.location = location;
+        this.events = events;
+    }
+
+    public Venue(Long id, String name, String location, long yelp_id) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.yelp_id = yelp_id;
+    }
+
+    public Venue(Long id, String name, String location, long yelp_id, List<Event> events) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.yelp_id = yelp_id;
+        this.events = events;
+    }
+
+
+
 
     public long getId() {
         return id;
@@ -47,5 +79,19 @@ public class Venue {
         this.location = location;
     }
 
+    public long getYelp_id() {
+        return yelp_id;
+    }
 
+    public void setYelp_id(long yelp_id) {
+        this.yelp_id = yelp_id;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
 }
