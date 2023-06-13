76775284f0ba4265def761a41d7ec5fde7353e0f
isaacachenbach <isaac.achenbach1@gmail.com>
2023-06-13 12:20:33 -0500

    working error pages

--- src/main/java/com/karaokehub/karaokehub/config/SecurityConfig.java
+++ src/main/java/com/karaokehub/karaokehub/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,13 +18,13 @@ public class SecurityConfig {
                 .requestMatchers( "/create-venue", "/user-profile", "/venue-profile", "/profile", "/profile/update").authenticated()
                 .requestMatchers("/register", "/login", "/index", "/logout", "/search-venue").permitAll()
                 .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
-        );
 
+        );
 //       http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
 
 
-        http.formLogin((form) -> form.loginPage("/login").defaultSuccessUrl("/index"));
-        http.logout((form) -> form.logoutSuccessUrl("/login"));
+        http.formLogin((form) -> form.loginPage("/login").defaultSuccessUrl("/profile"));
+        http.logout((form) -> form.logoutSuccessUrl("/search"));
         http.httpBasic(withDefaults());
         return http.build();
     }
@@ -32,4 +33,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
 }
