8fdb36374a0c703130b6f749245630dd102f9e3b
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-06-14 15:19:13 -0500

    venue-profile

--- src/main/resources/templates/yelp.js
+++ src/main/resources/templates/yelp.js
@@ -1,6 +1,15 @@
 (async () => {
-    const venueComments = {
-        "type": "FeatureCollection",
-    }
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer' + yelpKey,
+            }
+        };
+
+        fetch('https://api.yelp.com/v3/businesses/${venue.yelp_id}/reviews?limit=20&sort_by=yelp_sort', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
     }
 )
