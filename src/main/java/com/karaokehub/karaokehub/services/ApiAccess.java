e2bd3f17a7ad3cc5bf15ae4e421cf05cbd00a687
isaacachenbach <isaac.achenbach1@gmail.com>
2023-06-15 15:28:03 -0500

    merge

--- src/main/java/com/karaokehub/karaokehub/services/ApiAccess.java
+++ src/main/java/com/karaokehub/karaokehub/services/ApiAccess.java
@@ -1,16 +1,1 @@
-package com.karaokehub.karaokehub.services;
 
-public class ApiAccess {
-    private static String apiKey = "2GcKsiasG148oNxhkk8NC_db3KvAqwYYsDmWNxo2xswrAIG68XtQ53f_BhD402yNgQp2HtnpjTRwbcTMt_oJqgIHgXkaR0cxAdilrur6ZdzofNILZo9skANOTZJ_ZHYx";
-    private static String predictClientId ="jlxEAsYos3A";
-    private static String predictHqApi = "9tDfDgIcxvVYaCy_tQdHIQHF1X8yYjptO-Gn09Kp";
-    public static String getApiKey() {
-        return apiKey;
-    }
-    public static String getPredictClientId(){
-        return predictClientId;
-    }
-    public static String getPredictHqApi(){
-        return predictHqApi;
-    }
-}
