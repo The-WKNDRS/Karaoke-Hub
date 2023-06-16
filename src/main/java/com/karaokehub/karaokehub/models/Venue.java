deaa812f8ca9367a438b9ed92cd624da1b449c78
Matthew Lorber <matthew.lorber@gmail.com>
2023-06-16 11:12:55 -0500

    map search and browse venues are working

--- src/main/java/com/karaokehub/karaokehub/models/Venue.java
+++ src/main/java/com/karaokehub/karaokehub/models/Venue.java
@@ -32,29 +32,6 @@ public class Venue {
     @Column(length = 100)
     private String yelp_id;
 
-    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "venue")
-    private List<Event> events;
-
-    public Venue(long id, String name, String address, String city, String state, String zip_code, String website, String yelp_id, List<Event> events) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip_code = zip_code;
-        this.website = website;
-        this.yelp_id = yelp_id;
-        this.events = events;
-    }
-
-    public List<Event> getEvents() {
-        return events;
-    }
-
-    public void setEvents(List<Event> events) {
-        this.events = events;
-    }
-
     public Venue() {
     }
 
@@ -79,6 +56,14 @@ public class Venue {
         this.yelp_id = yelp_id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Venue(String name, String address, String city, String state, String zip_code, String website) {
         this.name = name;
         this.address = address;
@@ -94,22 +79,6 @@ public class Venue {
         this.city = city;
         this.state = state;
         this.zip_code = zip_code;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getAddress() {
@@ -158,5 +127,18 @@ public class Venue {
 
     public void setYelp_id(String yelp_id) {
         this.yelp_id = yelp_id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @PrePersist
+    public void generateYelpId() {
+        if (name != null && city != null) {
+            String sanitizedBusinessName = name.toLowerCase().replaceAll("[^a-z0-9]+", "-");
+            String sanitizedCity = city.toLowerCase().replaceAll("[^a-z0-9]+", "-");
+            yelp_id = sanitizedBusinessName + "-" + sanitizedCity;
+        }
     }
 }
--- src/main/java/com/karaokehub/karaokehub/repository/VenueRepository.java
+++ src/main/java/com/karaokehub/karaokehub/repository/VenueRepository.java
@@ -1,10 +1,17 @@
 package com.karaokehub.karaokehub.repository;
 
 import com.karaokehub.karaokehub.models.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
 
 public interface VenueRepository extends JpaRepository<Venue, Long> {
     Venue findById(long id);
+
+    @Query("SELECT v.address, v.city, v.name, v.state, v.website, v.yelp_id, v.zip_code FROM Venue v")
+    List<Object[]> fetchVenueDetails();
 
     @Query(
             value = "SELECT JSON_ARRAYAGG(JSON_OBJECT('id', v.id, 'address', v.address, 'city', v.city, 'name', v.name, 'state', v.state, 'website', v.website, 'yelp_id', v.yelp_id, 'zipcode', v.zip_code, 'events', e.events)) FROM venue v LEFT JOIN (SELECT venue_id, JSON_ARRAYAGG(JSON_OBJECT('id', id, 'day_of_week', day_of_week, 'dj', dj, 'end_time', end_time, 'start_time', start_time)) AS events FROM events GROUP BY venue_id) e ON e.venue_id = v.id",
