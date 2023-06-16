3921a91a4169bc093ca1eba2540bf643fe2f6c4b
Joe Beltran <jab102191@gmail.com>
2023-06-16 12:57:35 -0500

    search by day of week  fully functional

--- src/main/java/com/karaokehub/karaokehub/repository/VenueRepository.java
+++ src/main/java/com/karaokehub/karaokehub/repository/VenueRepository.java
@@ -19,4 +19,29 @@ public interface VenueRepository extends JpaRepository<Venue, Long> {
             nativeQuery = true
     )
     String findVenueByZipcodeNative(String zipcode);
+
+
+    @Query(
+            value = "SELECT JSON_ARRAYAGG(JSON_OBJECT('id', v.id, 'address', v.address, 'city', v.city, 'name', v.name, 'state', v.state, 'website', v.website, 'yelp_id', v.yelp_id, 'zipcode', v.zip_code, 'events', e.events)) FROM venue v LEFT JOIN (SELECT venue_id, JSON_ARRAYAGG(JSON_OBJECT('id', id, 'day_of_week', day_of_week, 'dj', dj, 'end_time', end_time, 'start_time', start_time)) AS events FROM events WHERE day_of_week = (\n" +
+                    "    CASE ?\n" +
+                    "        WHEN 'Monday' THEN 'Mon'\n" +
+                    "        WHEN 'Tuesday' THEN 'Tue'\n" +
+                    "        WHEN 'Wednesday' THEN 'Wed'\n" +
+                    "        WHEN 'Thursday' THEN 'Thu'\n" +
+                    "        WHEN 'Friday' THEN 'Fri'\n" +
+                    "        WHEN 'Saturday' THEN 'Sat'\n" +
+                    "        WHEN 'Sunday' THEN 'Sun'\n" +
+                    "        END\n" +
+                    "    ) GROUP BY venue_id) e ON e.venue_id = v.id WHERE v.zip_code = ? AND e.venue_id IS NOT NULL;",
+
+            nativeQuery = true
+    )
+    String filterVenueByWeekdayNative(String weekday, String zipcode);
+
+    @Query(
+            value ="SELECT JSON_ARRAYAGG(JSON_OBJECT('id', v.id, 'address', v.address, 'city', v.city, 'name', v.name, 'state', v.state, 'website', v.website, 'yelp_id', v.yelp_id, 'zipcode', v.zip_code, 'events', e.events)) FROM venue v LEFT JOIN (SELECT venue_id, JSON_ARRAYAGG(JSON_OBJECT('id', id, 'day_of_week', day_of_week, 'dj', dj, 'end_time', end_time, 'start_time', start_time)) AS events FROM events WHERE day_of_week = (CASE ? WHEN 'Monday' THEN 'Mon' WHEN 'Tuesday' THEN 'Tue' WHEN 'Wednesday' THEN 'Wed' WHEN 'Thursday' THEN 'Thu' WHEN 'Friday' THEN 'Fri' WHEN 'Saturday' THEN 'Sat' WHEN 'Sunday' THEN 'Sun' END) GROUP BY venue_id) e ON e.venue_id = v.id WHERE e.venue_id IS NOT NULL;",
+
+            nativeQuery = true
+    )
+    String filterAllByWeekdayNative(String weekday);
 }
