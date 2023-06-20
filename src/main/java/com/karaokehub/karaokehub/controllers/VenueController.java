0181c54bc162af7637f2eac43b9937737deead64
Joe Beltran <jab102191@gmail.com>
2023-06-20 15:45:21 -0500

    user comments on venues

--- src/main/java/com/karaokehub/karaokehub/controllers/VenueController.java
+++ src/main/java/com/karaokehub/karaokehub/controllers/VenueController.java
@@ -1,8 +1,8 @@
 package com.karaokehub.karaokehub.controllers;
-import com.karaokehub.karaokehub.models.Event;
-import com.karaokehub.karaokehub.models.Venue;
-import com.karaokehub.karaokehub.models.VenueEventDto;
+import com.karaokehub.karaokehub.models.*;
+import com.karaokehub.karaokehub.repository.CommentRepository;
 import com.karaokehub.karaokehub.repository.EventRepository;
+import com.karaokehub.karaokehub.repository.UserRepository;
 import com.karaokehub.karaokehub.repository.VenueRepository;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -21,10 +21,14 @@ public class VenueController {
 
     private final VenueRepository venueDao;
     private final EventRepository eventDao;
+    private final CommentRepository commentDao;
+    private final UserRepository userDao;
 
-    public VenueController(VenueRepository venueDao, EventRepository eventDao) {
+    public VenueController(VenueRepository venueDao, EventRepository eventDao, CommentRepository commentDao, UserRepository userDao) {
         this.venueDao = venueDao;
         this.eventDao = eventDao;
+        this.commentDao = commentDao;
+        this.userDao = userDao;
     }
 
     @GetMapping("/create-venue")
@@ -39,12 +43,26 @@ public class VenueController {
         return "redirect:venue-profile/" + venue.getId();
     }
 
-    @GetMapping("/venue-profile/{id}")
+    @GetMapping("/venue/{id}")
     public String venueProfile(Model model, @PathVariable long id) {
         model.addAttribute("venue", venueDao.getReferenceById(id));
         List<Event> events = eventDao.findByVenueId(id);
         model.addAttribute("events", events);
+        model.addAttribute("numComments", commentDao.findAllByVenueId(id).size());
+        if (commentDao.findAllByVenueId(id).size() > 0) {
+            model.addAttribute("comments", commentDao.findAllByVenueId(id));
+        }
         return "venue-profile";
+    }
+
+    @PostMapping("/venue/comment")
+    public String venueComment(@RequestParam(name="body") String body, @RequestParam(name="venue_id") long venue_id) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        long id = user.getId();
+        user = userDao.getReferenceById(id);
+        Venue venue = venueDao.getReferenceById(venue_id);
+        commentDao.save(new Comment(body, user, venue));
+        return "redirect:/venue/" + venue_id;
     }
 
     @PostMapping("/edit-venue/{id}")
